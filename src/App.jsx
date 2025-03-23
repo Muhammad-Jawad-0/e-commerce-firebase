12648e95faa259b937ae0d788529ba84a5073db5
Muhammad Jawad <muhammadjawadforgithub@gmail.com>
2025-03-24 01:42:16 +0500

    admin page create

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,12 @@ import HomePage from "./pages/home/HomePage";
 import NoPage from "./pages/noPage/NoPage";
 import ProductInfo from "./pages/productInfo/ProductInfo";
 import ScrollTop from "./components/scrollTop/ScrollTop";
+import CartPage from "./pages/cart/CartPage";
+import AllProduct from "./pages/allProduct/AllProduct";
+import Signup from "./pages/registration/Signup";
+import Login from "./pages/registration/Login";
+import UserDashboard from "./pages/user/UserDashboard";
+import AdminDashboard from "./pages/admin/AdminDashboard";
 
 function App() {
   return (
@@ -14,6 +20,12 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/*" element={<NoPage />} />
         <Route path="/productinfo" element={<ProductInfo />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/allproduct" element={<AllProduct />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/user-dashboard" element={<UserDashboard />} />
+        <Route path="/admin-dashboard" element={<AdminDashboard />} />
       </Routes>
       </BrowserRouter>
     </>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,58 +2,63 @@ import { Link } from "react-router-dom";
 import SearchBar from "../searchBar/SearchBar";
 
 const Navbar = () => {
-    // navList Data
-    const navList = (
-        <ul className="flex !space-x-3 text-white font-medium text-md !px-5 !gap-x-5">
-            {/* Home */}
-            <li>
-                <Link to={'/'}>Home</Link>
-            </li>
+  // navList Data
+  const navList = (
+    <ul className="flex !space-x-3 text-white font-medium text-md !px-5 !gap-x-5">
+      {/* Home */}
+      <li>
+        <Link to={"/"}>Home</Link>
+      </li>
 
-            {/* All Product */}
-            <li>
-                <Link to={'/allproduct'}>All Product</Link>
-            </li>
+      {/* All Product */}
+      <li>
+        <Link to={"/allproduct"}>All Product</Link>
+      </li>
 
-            {/* Signup */}
-            <li>
-                <Link to={'/signup'}>Signup</Link>
-            </li>
+      {/* Signup */}
+      <li>
+        <Link to={"/signup"}>Signup</Link>
+      </li>
 
-            {/* User */}
-            <li>
-                <Link to={'/'}>Kamal</Link>
-            </li>
+      {/* User */}
+      <li>
+        <Link to={"/user-dashboard"}>Kamal</Link>
+      </li>
 
-            {/* Cart */}
-            <li>
-                <Link to={'/cart'}>
-                    Cart(0)
-                </Link>
-            </li>
-        </ul>
-    )
-    return (
-        <nav className="bg-pink-600 sticky top-0 !h-12 flex justify-center flex-col">
-            {/* main  */}
-            <div className="lg:flex lg:justify-between items-center !py-3 lg:!px-3 ">
-                {/* left  */}
-                <div className="left !py-3 lg:!py-0">
-                    <Link to={'/'}>
-                        <h2 className=" font-bold text-white text-2xl text-center">E-Bharat</h2>
-                    </Link>
-                </div>
+      {/* Admin */}
+      <li>
+        <Link to={"/admin-dashboard"}>Admin</Link> {/* Admin Dashboard */}
+      </li>
 
-                {/* right  */}
-                <div className="right flex justify-center !mb-4 lg:!mb-0">
-                    {navList}
-                </div>
+      {/* Cart */}
+      <li>
+        <Link to={"/cart"}>Cart(0)</Link>
+      </li>
+    </ul>
+  );
+  return (
+    <nav className="bg-pink-600 sticky top-0 !h-12 flex justify-center flex-col">
+      {/* main  */}
+      <div className="lg:flex lg:justify-between items-center !py-3 lg:!px-3 ">
+        {/* left  */}
+        <div className="left !py-3 lg:!py-0">
+          <Link to={"/"}>
+            <h2 className=" font-bold text-white text-2xl text-center">
+              E-Bharat
+            </h2>
+          </Link>
+        </div>
 
-                {/* Search Bar  */}
-                <SearchBar />
-            </div>
-        </nav>
-    );
-}
+        {/* right  */}
+        <div className="right flex justify-center !mb-4 lg:!mb-0">
+          {navList}
+        </div>
+
+        {/* Search Bar  */}
+        <SearchBar />
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
